17ceb9169562db370c0a00c1162ca165b1b12857
sam-malanchuk <malanchuksam@gmail.com>
2019-06-29 14:06:46 -0400

    started styling the smurfs items, starting to build the circle of initials

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './Smurf.css';
 
 const Smurf = props => {
   return (
     <div className="Smurf">
+      <span className="circle">JK</span>
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
